6f8ac942d56821abb3656497f30ed79b8beb6df4
Loubnaa1 <Loubnamerzak4@gmail.com>
2023-08-11 00:12:23 +0000

    My_code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alph in range(97, 123):
-    if chr(alph) != '101' and chr(alph) != '113':
+    if chr(alph) != 101 and chr(alph) != 113:
         print("{}".format(chr(alph)), end="")
