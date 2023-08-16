aba547b7c0b70b99881699cb3efd0f2638ce30f2
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-15 17:09:51 -0700

    my_code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("{} argument:".format(length))
     else:
         print("{} arguments:".format(length))
-        for i in range(length):
+    for i in range(length):
             print("{}: {}".format(i+1, sys.argv[i+1])
