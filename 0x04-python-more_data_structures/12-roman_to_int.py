904132843a3ea7bd0bda69baf1c2c3dc425f4ff7
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-18 18:14:53 -0700

    My_code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,6 @@ def roman_to_int(roman_string):
     dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if not isinstance(roman_string, str):
         return 0
-    
     index = len(roman_string) - 1
     while index >= 0:
         i = roman_string[index]
@@ -15,6 +14,5 @@ def roman_to_int(roman_string):
             somme += numb
         else:
             somme -= numb
-        index -= 1
-    
-    return somme
+        index -= 1    
+    return (somme)
