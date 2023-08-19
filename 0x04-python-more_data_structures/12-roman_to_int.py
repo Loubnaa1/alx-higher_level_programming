52aa2f37b8ad31242044cf22238f0cdbd89e9862
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-18 18:36:41 -0700

    My_code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,16 +4,16 @@ def roman_to_int(roman_string):
         return 0
 
     somme = 0
-    dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dig = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     index = len(roman_string) - 1
     while index >= 0:
         i = roman_string[index]
-        number = dictionary[i]
-        if somme < number * 5:
-            somme += number
+        num = dig[i]
+        if somme < num * 5:
+            somme += num
         else:
-            somme -= number
+            somme -= num
         index -= 1
 
     return somme
