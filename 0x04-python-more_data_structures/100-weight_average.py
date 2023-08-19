550e4e6b021de43b0afc765459ed523b9ecf34c6
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-18 17:57:05 -0700

    My_code

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    numerator = 0
+    denominator = 0
+
+    for x in my_list:
+        produit = x[0] * x[1]
+        numerateur += produit
+        denominateur += x[1]
+
+    if denominateur == 0:
+        denominateur = 1
+
+    weighted_average = numerateur / denominateur
+    return weighted_average
+
