677237d1d7e0481a445abccce6acbabb13694211
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-18 17:48:32 -0700

    My_code

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,5 +2,5 @@
 def complex_delete(a_dictionary, value):
     for i in list(a_dictionary.keys()):
         if a_dictionary[i] is value:
-            a_dictionary.pop(list_key[i])
+            a_dictionary.pop(list(a_dictionary.keys()[i])
     return a_dictionary
