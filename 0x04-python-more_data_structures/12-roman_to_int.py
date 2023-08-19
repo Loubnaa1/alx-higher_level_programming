7451b1c2b7ad885e495dbabf99301a9d077b0968
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-18 18:30:04 -0700

    My_code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    
+
     total = 0
     digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    
+
     index = len(roman_string) - 1
     while index >= 0:
         r = roman_string[index]
@@ -16,5 +15,11 @@ def roman_to_int(roman_string):
         else:
             total -= num
         index -= 1
-    
+
     return total
+
+# Example usage
+roman_numeral = "IX"
+result = roman_to_int(roman_numeral)
+print(f"The integer value of {roman_numeral} is {result}")
+
