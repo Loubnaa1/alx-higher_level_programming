ae90dfca5e59efe7293846564c3814fbe09a5d74
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-18 18:08:58 -0700

    My_code

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    total = 0
+    num = 0
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not isinstance(roman_string, str):
+        return 0
+    
+    index = len(roman_string) - 1
+    while index >= 0:
+        r = roman_string[index]
+        num = digits[r]
+        if total < num * 5:
+            total += num
+        else:
+            total -= num
+        index -= 1
+    
+    return total
+
