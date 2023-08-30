92b140198e73c03c70ae596ad0a456c45f8d8c9b
Loubnaa1 <loubnamerzak4@gmail.com>
2023-08-30 03:32:45 +0200

    my_commit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,15 +3,15 @@
 
 
 class Square:
-    """ class Square"""
+    """ class Square:"""
     def __init__(self, size=0):
-        """ initial"""
+        """Initial"""
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
-def area(self):
-    """ returns the current square area"""
-    return (self.__size * 2)
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size * self.__size)
