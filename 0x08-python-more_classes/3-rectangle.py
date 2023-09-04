bef470de780585043e35fd84858535d29aef5e81
Loubnaa1 <loubnamerzak4@gmail.com>
2023-09-05 01:14:32 +0200

    my_code

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,14 +52,11 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (2 * (self.__width + self.__height))
+    
     def __str__(self):
         """Converting a rectangle object into a string"""
         if self.__width == 0 or self.__height == 0:
             return ("")
-
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+        else:
+            rectangle_lines = ["#" * self.__width for _ in range(self.__height)]
+            return ("\n".join(rectangle_lines))
