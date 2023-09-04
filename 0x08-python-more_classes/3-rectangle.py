7e14bd581849b574fd9f63678b15e3a1e024df37
Loubnaa1 <loubnamerzak4@gmail.com>
2023-09-05 01:08:34 +0200

    my_code

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -57,5 +57,5 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
         else:
-            rectangle_lines = ["#" * self.__width for _ in range(self.__height)]
+            rectangle_lines = ["#" * self.__width for i in range(self.__height)]
             return ("\n".join(rectangle_lines))
