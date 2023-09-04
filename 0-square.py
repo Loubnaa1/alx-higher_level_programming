a3eddc708498badc014308e55e3d097dd150fd8b
Loubnaa1 <loubnamerzak4@gmail.com>
2023-09-05 01:56:34 +0200

    my_code

--- 0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""create empty class Square that defines a square:"""
-
-
-class Square:
-    """ Empty class """
-    pass
--- 1-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""create class Square that defines a square:"""
-
-
-class Square:
-    """Represent a square."""
-    def __init__(self, size=0):
-        """initial"""
-        self.__size = size
