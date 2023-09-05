09f0739250c82a5ed69a87b2508712075ee10c2e
Loubnaa1 <loubnamerzak4@gmail.com>
2023-09-05 02:04:29 +0200

    my_code

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -81,6 +81,7 @@ class Rectangle:
 
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """ static method that returns the biggest rectangle
