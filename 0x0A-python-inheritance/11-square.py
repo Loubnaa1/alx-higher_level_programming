af6aac44c00e543ba1497b50b0255ce848fe693e
Loubnaa1 <loubnamerzak4@gmail.com>
2023-09-12 02:51:45 +0200

    my_code

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,7 +8,14 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initialization a new square."""
-        
+
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+    
+    def area(self):
+        """ calculates the square area """
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
