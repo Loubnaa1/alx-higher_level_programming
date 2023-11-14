a824692b64e0e82f10a72d5f0e87c4c9fc41a7c7
Loubnaa1 <loubnamerzak4@gmail.com>
2023-11-14 02:54:53 +0100

    my_proj

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const x = Number(process.argv[2]);
-if (isNaN(parseInt(argv[2]))) {
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   let i = 0;
-  while (i < x ) {
+  while (i < x) {
     console.log('C is fun');
     i++;
   }
