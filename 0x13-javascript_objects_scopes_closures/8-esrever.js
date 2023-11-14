0b2cba6a12a3f372be441789dcdbe6b7e4fe3cdb
Loubnaa1 <loubnamerzak4@gmail.com>
2023-11-14 17:09:49 +0100

    my_proj

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   let len = list.length - 1;
-  let i = 0;
+  let n = 0;
   while (len > 0 && len > i) {
     const num = list[len];
     list[len] = list[n];
     list[n] = num;
-    i++;
+    n++;
     len--;
   }
   return list;
