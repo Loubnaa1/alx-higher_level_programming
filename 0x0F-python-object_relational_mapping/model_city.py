461830fe9dea68a15d9afe770f24b239bdcf3293
Loubnaa1 <loubnamerzak4@gmail.com>
2023-11-20 00:57:59 +0100

    my_proj

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class City(Base):
     """creating City class"""
-    
+
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
