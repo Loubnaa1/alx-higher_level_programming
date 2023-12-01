d729bfe25721abd311024d20afbf6677e4748551
Loubnaa1 <loubnamerzak4@gmail.com>
2023-11-30 18:31:25 -0800

    python_network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
-
+"""find_peak function script"""
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
+    """Function to find a peak element in an unsorted list of integers."""
 
-    if list_of_integers is None or list_of_integers == []:
+    a, b = 0, len(list_of_integers) - 1
+
+    if b == -1:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    while a < b:
+        tmp = (a + b) // 2
+        if list_of_integers[tmp] < list_of_integers[tmp + 1]:
+            a = tmp + 1
+        else:
+            b = tmp
+
+    return list_of_integers[a]
