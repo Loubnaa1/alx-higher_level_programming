8ec0dd9a5529c27889018dbcd1761b5c552deeb3
Loubnaa1 <loubnamerzak4@gmail.com>
2023-12-01 18:44:43 -0800

    python-network_1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+if __name__ == "__main__":
+    request = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type:", type(request.text))
+    print("\t- content:", request.text)
