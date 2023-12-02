2aebd605735c331a33e95aa71691ab9b786f3e5d
Loubnaa1 <loubnamerzak4@gmail.com>
2023-12-01 18:47:37 -0800

    python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+"""script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 if __name__ == '__main__':
-    
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        core = response.read()
+    print("Body response:")
+    print("\t- type:", type(core))
+    print("\t- content:", core)
+    print("\t- utf8 content:", core.decode('utf-8'))
