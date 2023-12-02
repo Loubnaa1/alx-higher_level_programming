1e98c82aed68f509409de99b896e36cd696844d0
Loubnaa1 <loubnamerzak4@gmail.com>
2023-12-01 20:37:36 -0800

    pro

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """list 10 commits (from the most recent to oldest)"""
-import sys
+from sys import argv
 import requests
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits"
-    url1 = url.format(sys.argv[2],sys.argv[1])
-    request = requests.get(url1)
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    request = requests.get(url)
     commits = request.json()
     try:
         for i in range(10):
