de32247bc5926f9068a91d5f72b551cd4de9b9c3
Loubnaa1 <loubnamerzak4@gmail.com>
2023-12-01 18:57:40 -0800

    python-network_1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response."""
+"""takes in a URL and an email, sends a POST request , and displays the body"""
 import sys
 import urllib.parse
 import urllib.request
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the body of the response."""
+"""takes in a URL, sends a request to the URL and displays the body of the response."""
 import sys
 from urllib import request, error
 
