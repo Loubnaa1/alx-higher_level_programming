34cc810d790b1b6278ea4786cf8c5ff7a8aa194b
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-03 17:46:58 -0800

    tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,9 +5,8 @@ request(process.argv[2], (error, response, body) => {
   if (error) console.error(error);
   else {
     const results = JSON.parse(body).results;
-    count = results.reduce((count, object) => {
-      return count + object.characters.find(character => character.endsWith('/18/'));
-    }, count);
+    console.log(results.filter(object => object.characters.some(character => character.endsWith('/18/'))
+  ).length);
   }
   console.log(count);
 });
