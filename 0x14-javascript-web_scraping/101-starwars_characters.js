ce8411c456bdf0032a70c0907417d4181d62e526
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-03 19:05:19 -0800

    tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,19 +2,19 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 request(url + process.argv[2], (error, response, body) => {
-  if (error) console.error('Error:', error);
+  if (error) console.error(error);
   else {
-    const caracters = JSON.parse(body).characters;
-    printcaract(caracters, 0);
+    const car = JSON.parse(body).characters;
+    printcar(car, 0);
   }
 });
-function printcaract (caracters, index) {
-  request(caracters[index], (error, response, body) => {
+function printcar (car, i) {
+  request(car[i], (error, response, body) => {
     if (error) console.error(error);
     else {
       console.log(JSON.parse(body).name);
-      if (index + 1 < caracters.length) {
-        printcaract(caracters, index + 1);
+      if (i + 1 < car.length) {
+        printcar(car, i + 1);
       }
     }
   });
