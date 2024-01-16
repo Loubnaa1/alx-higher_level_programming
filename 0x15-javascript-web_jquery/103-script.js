204023456ceec8c78b0373743ae4bc31294c9e3f
Loubnaa1 <loubnamerzak4@gmail.com>
2024-01-15 16:50:10 -0800

    all_project

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,20 @@
-kkk
+$(document).ready(function() {
+
+    function getAndShowHello() {
+        var lang = $('#language_code').val();
+        var url = 'https://www.fourtonfish.com/hellosalut/?lang=' + lang;
+
+        $.get(url, function(data) {
+            $('#hello').text(data.hello);
+        });
+    }
+
+
+    $('#btn_translate').click(getAndShowHello);
+
+    $('#language_code').keypress(function(event) {
+        if (event.keyCode === 13) {
+            getAndShowHello();
+        }
+    });
+});
